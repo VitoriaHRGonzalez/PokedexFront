df16809c4cf9cf7917fdf75872f09d20910ab0c7
VitoriaGonzalez <vitoria.gonzalez@poatek.com>
2024-09-11 11:12:20 -0300

    A

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
- web: npm startweb: node src/heroku-start.js
+ web: npm start
--- src/heroku-start.js
+++ src/heroku-start.js
@@ -1,18 +1,18 @@
-// Crie um novo arquivo scripts/heroku-start.js
-import express, { json } from 'express';
-const aplicativo = express(); 
-const porta = process.env.PORT || 3000;
-aplicativo.use(json());
-// Sua pasta de ativos estáticos de pré-compilação 
-app.use(static(path.join(__dirname, '..', 'dist')));
-// Root redireciona para os ativos de pré-compilação 
-app.get('/', function(req,res){ 
-  res.sendFile(path.join(__dirname, '..', 'dist')); 
-});
-// Qualquer página redireciona para a pasta de ativos de pré-compilação index.html que // carregará o aplicativo React 
-app.get('*', function(req,res){ 
-  res.sendFile(path.join(__dirname, '..', 'dist/index.html')); 
-});
-app.listen(porta, ()=>{ 
-  console.log("O servidor está sendo executado na porta: ", porta) 
-})
+// import express, { json } from 'express';
+
+// const aplicativo = express(); 
+// const porta = process.env.PORT || 3000;
+// aplicativo.use(json());
+// // Sua pasta de ativos estáticos de pré-compilação 
+// app.use(static(path.join(__dirname, '..', 'dist')));
+// // Root redireciona para os ativos de pré-compilação 
+// app.get('/', function(req,res){ 
+//   res.sendFile(path.join(__dirname, '..', 'dist')); 
+// });
+// // Qualquer página redireciona para a pasta de ativos de pré-compilação index.html que // carregará o aplicativo React 
+// app.get('*', function(req,res){ 
+//   res.sendFile(path.join(__dirname, '..', 'dist/index.html')); 
+// });
+// app.listen(porta, ()=>{ 
+//   console.log("O servidor está sendo executado na porta: ", porta) 
+// })
