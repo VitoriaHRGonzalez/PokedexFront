cc51b9bb89de81509e44532578af49b208581cfd
VitoriaGonzalez <vitoria.gonzalez@poatek.com>
2024-09-12 11:28:17 -0300

    baseURL direto com o endereço do backend

--- src/services/trainers.jsx
+++ src/services/trainers.jsx
@@ -1,4 +1,4 @@
-export const baseURL = process.env.REACT_APP_API_URL;
+export const baseURL = 'https://pokedexbackend-2c9bfccb0af0.herokuapp.com/api';
 const ImageBaseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork';
 
 // Tirei o Api da URL no Heroku
